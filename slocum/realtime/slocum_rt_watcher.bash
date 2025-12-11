67f2206a0423da43978e5a0d789e26a179594cf3
cnrdh <cnrdh@users.noreply.github.com>
2025-12-11 21:14:20 +0100

    remove debug and cleanup

--- slocum/realtime/slocum_rt_watcher.bash
+++ slocum/realtime/slocum_rt_watcher.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
-# Real-time detection of incoming files from a Slocum,
-# restricted to compressed binary dinkum, 
-# ie. files  with extensions: .dcd .ecd .scd .tcd
-#
-# Run: source .env && ./slocum_rt_watcher.bash
+# Real-time watcher for incoming Slocum files (compressed dinkum binary)
+# 
+# Documentation:
+# https://github.com/akvaplan-niva/gliderdata/blob/main/slocum/realtime/readme.md
 #
 # Requirements
 # * inotifywait # apt install inotify-tools
@@ -11,7 +10,8 @@
 # Links on inotify
 # https://github.com/inotify-tools/inotify-tools/wiki
 # https://developer.ibm.com/tutorials/l-inotify/ 
-
+#
+# FIXME: Add timeout param
 watch_dir="${1:-"/var/opt/gmc/gliders"}"
 mission_dir="${2:-${RTD_MISSION_DIR:-}}"
 mission_blob="${3:-${RTD_MISSION_BLOB:-}}"
@@ -25,21 +25,20 @@ usage="Usage: $cmd [watch-dir] [mission-dir] [mission-blob]"
 slocum_rt_pipeline="$here/slocum_rt_pipeline_xcd_file.bash"
 [[ ! -x "$slocum_rt_pipeline" ]] && { echo "ERROR missing: $slocum_rt_pipeline"; exit 1; }
 
-echo "[$(date -Is)] Slocum RT watcher active for $watch_dir"
-echo "[$(date -Is)] Mission dir: $mission_dir"
-echo "[$(date -Is)] Mission blob: $mission_blob"
-
+# echo "[$(date -Is)] Slocum RT watcher active for $watch_dir"
+# echo "[$(date -Is)] Mission dir: $mission_dir"
+# echo "[$(date -Is)] Mission blob: $mission_blob"
 inotifywait -m -r "$watch_dir" \
-    --quiet \
-    --timeout 600 \
-    --event close_write \
-    --event moved_to \
-    --include '\.(dcd|ecd|scd|tcd)$' \
-    --format '%w %f %e' |
+  --quiet \
+  --timeout 600 \
+  --event close_write \
+  --event moved_to \
+  --include '\.(dcd|ecd|scd|tcd)$' \
+  --format '%w %f %e' |
 while read -r dir file event; do
-  echo "[$(date -Is)] <- $dir$file [$event]"
+  echo "[$(date -Is)] Slocum RT watcher <- $dir$file [$event]"
   if [[ "$dir" =~ /([^/]+)/(from-glider) ]]; then
-      glider_id="${BASH_REMATCH[1]}"
-      "$slocum_rt_pipeline" "$glider_id" "$dir$file" "$mission_dir" "$mission_blob"
+    glider_id="${BASH_REMATCH[1]}"
+    "$slocum_rt_pipeline" "$glider_id" "$dir$file" "$mission_dir" "$mission_blob"
   fi
 done
